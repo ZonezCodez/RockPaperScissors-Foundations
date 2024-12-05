46ceb6aacd3e9085bb1bab9babb0cd2f549a5fd0
Zone_Codez <odincodezone@gmail.com>
2024-12-05 02:39:48 -0600

    Worked on computer logic and player logic for rps

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Get either rock,paper or scissors as a return value when called.
 function getcomputerChoice(){
-    
+    const board = ['rock','paper','scissors'];
+    let choice = board[Math.floor(Math.random()*3)];
+    return choice;
 }
+// Gets users choice and returns it.
+function gethumanChoice(){
+    let choice = prompt('Rock,Paper,Scissors?');
+    choice = choice.toLowerCase();
+    if (choice === 'rock' || choice === 'paper' || choice === 'scissors'){
+        return choice;
+    }else{
+        gethumanChoice();
+    }
+}
+
