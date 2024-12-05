445ba4c1b204a1ed431f41b4feea6348fb3d9f84
Zone_Codez <odincodezone@gmail.com>
2024-12-05 02:51:13 -0600

    Added round functionality so you can play rounds

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// Variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Logic for a single round of play.
+function playRound(player,computer){
+    if(player === computer){
+        tieRound();
+    }else if(player === 'rock' && computer === 'scissors'){
+        roundWon();
+    }else if(player === 'paper' && computer === 'rock'){
+        roundWon();
+    }else if(player === 'scissors' && computer === 'paper'){
+        roundWon();
+    }else{
+        roundLost()
+    }
+}
+// Handles rounds where the player wins
+function roundWon(){
+    console.log('You win!');
+}
+// Handles rounds where the player ties
+function tieRound(){
+    console.log('You got a tie!');
+}
+//Handles rounds where player loses
+function roundLost(){
+    console.log('You lost!')
+}
 // Get either rock,paper or scissors as a return value when called.
 function getcomputerChoice(){
     const board = ['rock','paper','scissors'];
