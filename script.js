dc8a5b52b65ffa64d31035a0a99e5f2157ff5dbe
Zone_Codez <odincodezone@gmail.com>
2024-12-05 03:24:08 -0600

    Finished rps game logic and round logic

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
 // Variables
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 5;
+let roundsPlayed = 0;
 
-// Logic for a single round of play.
-function playRound(player,computer){
-    if(player === computer){
-        tieRound();
-    }else if(player === 'rock' && computer === 'scissors'){
-        roundWon();
-    }else if(player === 'paper' && computer === 'rock'){
-        roundWon();
-    }else if(player === 'scissors' && computer === 'paper'){
-        roundWon();
-    }else{
-        roundLost()
-    }
-}
-// Handles rounds where the player wins
-function roundWon(){
-    console.log('You win!');
-}
-// Handles rounds where the player ties
-function tieRound(){
-    console.log('You got a tie!');
-}
-//Handles rounds where player loses
-function roundLost(){
-    console.log('You lost!')
-}
 // Get either rock,paper or scissors as a return value when called.
 function getcomputerChoice(){
     const board = ['rock','paper','scissors'];
@@ -44,4 +20,52 @@ function gethumanChoice(){
         gethumanChoice();
     }
 }
+// Logic for rounds of play.
+function playRound(player,computer){
+    // Game logic handles here
+    if(player === computer){
+        tieRound(player,computer);
+    }else if(player === 'rock' && computer === 'scissors'){
+        roundWon(player,computer);
+    }else if(player === 'paper' && computer === 'rock'){
+        roundWon(player,computer);
+    }else if(player === 'scissors' && computer === 'paper'){
+        roundWon(player,computer);
+    }else{
+        roundLost(player,computer);
+    }
+}
+// Handles rounds where the player wins
+function roundWon(p,c){
+    console.log(`You win ${p} beats ${c}!`);
+    humanScore++;
+}
+// Handles rounds where the player ties
+function tieRound(p,c){
+    console.log(`${p} equals ${c} its a tie!`);
+}
+//Handles rounds where player loses
+function roundLost(p,c){
+    console.log(`You lost ${p} loses to ${c}!`)
+    computerScore++;
+}
+// This function finishes the game and reports each score!
+function finishGame(p,c){
+    if(p < c){
+        console.log(`You lost ${c} to ${p}!`);
+    }else if(c < p) {
+        console.log(`You won ${p} to ${c}!`);
+    }else{
+        console.log(`Tie Game! ${c} to ${p}`);
+    }
+}
 
+// This log handles the game loop you can set custom amount of rounds to play.
+while(roundsPlayed < rounds){
+    roundsPlayed++;
+    playRound(gethumanChoice(),getcomputerChoice());
+}
+// This logic handles calling a game over
+finishGame(humanScore,computerScore);
+
+    
